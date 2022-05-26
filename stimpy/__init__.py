c06385b2b70fd11016272b05528dc6529d56e011
Jeff Weiss <jmw182@pitt.edu>
2022-05-26 15:56:49 -0400

    Updated __init__.py documentation.

--- stimpy/__init__.py
+++ stimpy/__init__.py
@@ -1,6 +1,30 @@
 # Author: Jeff Weiss <jeff.weiss@pitt.edu>
 # May 2022
 
-"""stimpy: a python package to interface with Blackrock cerestim API."""
+"""stimpy: a python package to interface with Blackrock cerestim API.
 
-__all__ = ['stimulator' '_bstimulator']
+Example:
+
+```
+from stimpy import stimulator
+
+stim_obj = stimulator.stimulator()
+print(stim_obj.lib_version())
+stim_obj.connect()
+stim_obj.configure_stimulus_pattern(
+    configID=1,
+    afcf=stimulator.wf_types.wf_cathodic_first,
+    pulses=1,
+    amp1=10,
+    amp2=10,
+    width1=200,
+    width2=200,
+    frequency=100,
+    interphase=100
+)
+stim_obj.manual_stimulus(electrode=1, configID=1)
+stim_obj.disconnect()
+```
+"""
+
+__all__ = ['stimulator','_bstimulator']
