790005283872091f7a9054b84827b5321f0ad5f6
Jeff Weiss <jmw182@pitt.edu>
2022-05-27 11:50:55 -0400

    Added simple example

---
+++ examples/example.py
@@ -0,0 +1,18 @@
+from stimpy import stimulator
+
+stim_obj = stimulator.stimulator()
+print(stim_obj.lib_version())
+stim_obj.connect()
+stim_obj.configure_stimulus_pattern(
+    configID=1,
+    afcf=stimulator.wf_types.wf_cathodic_first,
+    pulses=1,
+    amp1=10,
+    amp2=10,
+    width1=200,
+    width2=200,
+    frequency=100,
+    interphase=100
+)
+stim_obj.manual_stimulus(electrode=1, configID=1)
+stim_obj.disconnect()
